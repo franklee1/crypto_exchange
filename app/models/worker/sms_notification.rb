d49702cb50dcb5919c5bf03738402f192dc8968e
powerdev0510 <power.dev2017@gmail.com>
2018-01-19 15:53:05 +0800

    authy_phone_verification_try

--- app/models/worker/sms_notification.rb
+++ app/models/worker/sms_notification.rb
@@ -2,23 +2,45 @@ module Worker
   class SmsNotification
 
     def process(payload, metadata, delivery_info)
-
+      payload.symbolize_keys!
       puts "asdfasdfsdf"
 
-      payload.symbolize_keys!
-      raise "TWILIO_NUMBER not set" if ENV['TWILIO_NUMBER'].blank?
+      puts payload
+      
+      puts Phonelib.parse(payload[:phone]).national
 
-      twilio_client.account.sms.messages.create(
-        from: ENV["TWILIO_NUMBER"],
-        to:   Phonelib.parse(payload[:phone]).international,
-        body: payload[:message]
-      )
+      
+      authy = Authy::API.register_user(:cellphone => Phonelib.parse(payload[:phone]).national, :country_code => "86")
+
+      if authy.ok?
+        self.authy_id = authy.id # this will give you the user authy id to store it in your database
+      else
+        authy.errors # this will return an error hash
+      end
+
+      response = Authy::API.request_sms(:id => user.authy_id)
+
+      if response.ok?
+        # sms was sent
+      else
+        response.errors
+        #sms failed to send
+      end
+
+      # payload.symbolize_keys!
+      # raise "TWILIO_NUMBER not set" if ENV['TWILIO_NUMBER'].blank?
+
+      # twilio_client.account.sms.messages.create(
+      #   from: ENV["TWILIO_NUMBER"],
+      #   to:   Phonelib.parse(payload[:phone]).international,
+      #   body: payload[:message]
+      # )
       
     end
 
-    def twilio_client
-      Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"], ssl_verify_peer: false
-    end
+    # def twilio_client
+    #   Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"], ssl_verify_peer: false
+    # end
 
   end
 end
--- config/initializers/authy.rb
+++ config/initializers/authy.rb
@@ -1,2 +1,4 @@
-Authy.api_key = ENV['AUTHY_API_KEY']
+require 'authy'
+
+Authy.api_key = 'ZcHNkcP944q4zbFDFkrv2fNXi5PT5C5G'
 Authy.api_uri = 'https://api.authy.com/'
