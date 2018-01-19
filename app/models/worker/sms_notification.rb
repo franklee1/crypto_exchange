fbacd6e5d760d7f1de7111142a26ad69c72b7dc7
powerdev0510 <power.dev2017@gmail.com>
2018-01-19 16:32:49 +0800

    authy_cell_verify_done

--- app/models/worker/sms_notification.rb
+++ app/models/worker/sms_notification.rb
@@ -13,19 +13,21 @@ module Worker
       authy = Authy::API.register_user(:cellphone => Phonelib.parse(payload[:phone]).national, :country_code => "86")
 
       if authy.ok?
-        self.authy_id = authy.id # this will give you the user authy id to store it in your database
+        
+        response = Authy::API.request_sms(:id => authy.id)
+
+        if response.ok?
+          # sms was sent
+          puts "Message Sent Correctly"
+        else
+          response.errors
+          #sms failed to send
+        end
       else
         authy.errors # this will return an error hash
       end
 
-      response = Authy::API.request_sms(:id => user.authy_id)
-
-      if response.ok?
-        # sms was sent
-      else
-        response.errors
-        #sms failed to send
-      end
+      
 
       # payload.symbolize_keys!
       # raise "TWILIO_NUMBER not set" if ENV['TWILIO_NUMBER'].blank?
